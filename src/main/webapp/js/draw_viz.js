a924dca570bbe759fcfeef66c8db94bee79bc80e
Saksa <eric.saksa@ES200873ML.i3domain.inin.com>
2015-07-23 13:21:45 -0400

    formatting viz chart

--- src/main/webapp/js/draw_viz.js
+++ src/main/webapp/js/draw_viz.js
@@ -1,33 +1,70 @@
 function testViz(id, data) {
-      var width = 500;
-      var testData = [
-        {label: "person a", times: [{"starting_time": 1355752800000, "ending_time": 1355759900000}, {"starting_time": 1355767900000, "ending_time": 1355774400000}]},
-        {label: "person b", times: [{"starting_time": 1355759910000, "ending_time": 1355761900000}, ]},
-        {label: "person c", times: [{"starting_time": 1355761910000, "ending_time": 1355763910000}]},
-      ];
+    var width = 600;
+    var testData = [
+        {label: "testA", times: [{"starting_time": 1355752800000, "ending_time": 1355759900000}, {"starting_time": 1355767900000, "ending_time": 1355774400000}]},
+        {label: "testB", times: [{"starting_time": 1355759910000, "ending_time": 1355761900000}, ]},
+        {label: "testC", times: [{"starting_time": 1355761910000, "ending_time": 1355763910000}]},
+        {label: "testD", times: [{"starting_time": 1355759800000, "ending_time": 1355761900000}, ]},
+        {label: "testE", times: [{"starting_time": 1355759910000, "ending_time": 1355761900000}, ]},
+        {label: "testF", times: [{"starting_time": 1355759970000, "ending_time": 1355761900000}, ]}
+    ];
 
-        var chart = d3.timeline()
-          .width(width)
-          .stack()
-          .margin({left:70, right:30, top:0, bottom:0})
-          .hover(function (d, i, datum) {
-          // d is the current rendering object
-          // i is the index during d3 rendering
-          // datum is the id object
+    var chart = d3.timeline()
+        .width(width)
+        .stack()
+        .margin({left:70, right:30, top:0, bottom:0})
+        .hover(function (d, i, datum) {
+        // d is the current rendering object
+        // i is the index during d3 rendering
+        // datum is the id object
             var div = $('#hoverRes');
             var colors = chart.colors();
             div.find('.coloredDiv').css('background-color', colors(i))
             div.find('#name').text(datum.label);
-          })
-          .click(function (d, i, datum) {
+        })
+        .click(function (d, i, datum) {
             alert(datum.label);
-          });
-          /*
-          .scroll(function (x, scale) {
-            $("#scrolled_date").text(scale.invert(x) + " to " + scale.invert(x+width));
-          });*/
-        var svg = d3.select("#"+id).append("svg").attr("width", width)
-          .datum(testData).call(chart);
+        })
+        .tickFormat({
+            format: d3.time.format("%I %p"),
+            tickTime: d3.time.hours,
+            tickInterval: 1,
+            tickSize: (0-testData.length)
+        });
+    var svg = d3.select("#"+id).append("svg").attr("width", width).datum(testData).call(chart);
 
+//    //vertical lines
+//    svg.selectAll(".vline").data(d3.range(26)).enter()
+//        .append("line")
+//        .attr("x1", function (d) {
+//        return d * 20;
+//    })
+//        .attr("x2", function (d) {
+//        return d * 20;
+//    })
+//        .attr("y1", function (d) {
+//        return 0;
+//    })
+//        .attr("y2", function (d) {
+//        return 500;
+//    })
+//        .style("stroke", "#eee");
+//
+//    // horizontal lines
+//    svg.selectAll(".vline").data(d3.range(26)).enter()
+//        .append("line")
+//        .attr("y1", function (d) {
+//        return d * 20;
+//    })
+//        .attr("y2", function (d) {
+//        return d * 20;
+//    })
+//        .attr("x1", function (d) {
+//        return 0;
+//    })
+//        .attr("x2", function (d) {
+//        return 500;
+//    })
+//        .style("stroke", "#eee");
 }
 
